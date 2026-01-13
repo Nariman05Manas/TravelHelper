8fd72cd7cb9791e8b44c1467de7db5d2da120690
IbragimovApps <Nbps4@list.ru>
2026-01-13 20:36:56 +0300

    Initial Commit

---
+++ TravelHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelHelper
+//
+//  Created by qwerty on 13.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelHelper/TravelHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelHelperApp.swift
+//  TravelHelper
+//
+//  Created by qwerty on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
